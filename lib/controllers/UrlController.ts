f0c99c3fc755e24794e82b6ed5f590c1986dd063
Glendon Philipp <gpbaculio@gmail.com>
2019-06-03 16:47:01 +0800

    add url test

--- lib/controllers/UrlController.ts
+++ lib/controllers/UrlController.ts
@@ -1,9 +1,18 @@
 import { Request, Response } from 'express';
 
 export default class WhoAmIController {
+  public isValidURL(url: string) {
+    const test = url.match(
+      /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+    );
+    return test === null ? false : true;
+  }
   public newUrl = (req: Request, res: Response) => {
-    console.log('req body', req.body);
-    console.log('req params', req.params);
-    res.json('hehe');
+    const { url: original_url } = req.body;
+    if (this.isValidURL(original_url)) {
+      res.json({ original_url });
+    } else {
+      res.json({ error: 'invalid URL' });
+    }
   };
 }
